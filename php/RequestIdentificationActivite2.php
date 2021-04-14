68430020d4031c6d5b162ca44d034a191369ab7f
CaroleC <carole.carre@gmail.com>
2021-04-14 14:38:45 +0200

    Suppression lignes de test

--- php/RequestIdentificationActivite2.php
+++ php/RequestIdentificationActivite2.php
@@ -17,28 +17,18 @@
 				//----------------------------------------------------------------------
 				//------  Envoyer la requête    ----------------------------------
 				$requete = "SELECT pwd FROM user WHERE pseudo = '$identifiant' AND pwd = '$password'";
-					//$requete = "SELECT * FROM user WHERE pwd = '".$password."'";
-					//---- Préparation de la requête
-					echo"Requête envoyée : $requete";
-					$resultat = $id -> query($requete);			//---- Envoie de la requête
+				$resultat = $id -> query($requete);			//---- Envoie de la requête
 
 				//----------------------------------------------------------------------
 				//------  Exploiter les résultats  --------------------------------
 					$row = $resultat->fetch_array(MYSQLI_ASSOC);
 					if ($row['pwd']=="LeMotDePasseEstBrav0"){
-						/*$message = "C'est le bon mot de passe !";
-						echo"Contenu de row[0] : $row[0]";
-						echo "$message";*/
 						echo '<script type="text/javascript">
 						document.location.href="http://127.0.0.1/tp-snir1/Activite3.html"</script>';
 					}
 					else{
-						/*$message = "Ce n'est pas le bon mot de passe !";
-						echo"Contenu de row[0] : $row[0]";
-						echo "$message";*/
 						echo '<script type="text/javascript">
 						document.location.href="http://127.0.0.1/tp-snir1/Activite2.html"</script>';
-
 						exit();
 					}
 					//----------------------------------------------------------------------
